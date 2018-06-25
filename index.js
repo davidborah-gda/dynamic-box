49af4e778ff754717ece0b777ddbbbd1f62abba9
David Borah <dborah87@gmail.com>
2018-06-25 18:56:09 -0400

    fixed the java index and the App finally works. Next is styling update!

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const App = {
     count: 0,
+    colors: ['red', 'green', 'blue'],
+
     init(){
       this.cacheDom();
       this.bindEventListeners();
@@ -9,42 +11,37 @@ const App = {
       this.root = document.querySelector('#app');
       this.forwardBtn = this.root.querySelector('.forward');
       this.backBtn = this.root.querySelector('.back');
-      this.display = this.root.querySelector('.display');
+      this.display = this.root.querySelector('.color-box');
     },
     bindEventListeners(){
       this.forwardBtn.addEventListener('click', this.goForward.bind(this));
       this.backBtn.addEventListener('click', this.goBack.bind(this));
     },
-    goBack(){
-      this.count -= 1;
-      this.render();
-      //if count = 0,3,6,9,12 show RED + or -
-      //if count = 1,4,7,10,13 show GREEN + or -
-      //if count = 2,5,8,11, 14 show BLUE + or -
 
-
-    },
     goForward(){
-      this.count += 1;
+      this.count = (this.count + 1 + this.colors.length) % this.colors.length;
       this.render();
     },
+
+    goBack(){
+      this.count = (this.count - 1 + this.colors.length) % this.colors.length;
+      this.render();
+
+    },
+
+   
     render(){
-     this.display.textContent = this.count;
 
-     if {
-         //(index + arr.lenght) % arr.length = 0 SHOW RED
-     }
-     else if {
-         //(index + arr.lenght) % arr.length = 1 SHOW GREEN
-     }
+     this.display.style.backgroundColor = this.colors[this.count];
+    
 
-     else if {
-         //(index + arr.lenght) % arr.length = 2 SHOW BLUE
-     }
+     
 
-     else {
-         console.log('Do you need some help?')
-     }
+
+
+    //  else {
+    //      console.log('Do you need some help?')
+    //  }
 
     }
   };
